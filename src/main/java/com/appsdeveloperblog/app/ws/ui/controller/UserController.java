9513be157a9f991c3d2db1de53cf8497dc996cb0
Kento75 <kento2github@gmail.com>
2019-07-30 10:25:07 +0900

    Add getUsers route

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -6,10 +6,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("users") // http://localhost:8080/users
 public class UserController {
 
-    @GetMapping
-    public String getUser() {
+    @GetMapping()
+    public String getUsers(@RequestParam(value="page", defaultValue = "1") int page,
+                           @RequestParam(value="limit", defaultValue = "50") int limit,
+                           @RequestParam(value="sort", defaultValue = "desc", required = false) String sort) {
 
-        return "get user was called";
+        return "get users was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
+    }
+
+    @GetMapping(path="/{userId}")
+    public String getUser(@PathVariable String userId) {
+
+        return "get user was called with userID = " + userId;
     }
 
     @PostMapping
